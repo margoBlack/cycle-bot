8c9e1a1e72d8397b8ed54f44c4b5e9646d1d8f58
Marharyta Hrinchenko <marharyta.hrinchenko@yalantis.net>
2024-05-29 18:53:14 +0100

    add hydrate and new logic

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 require('dotenv').config();
-const { Bot, Keyboard } = require('grammy');
+const { Bot, Keyboard, InlineKeyboard } = require('grammy');
+const { hydrate } = require('@grammyjs/hydrate');
 
 const bot = new Bot(process.env.BOT_API_KEY);
+bot.use(hydrate());
+
+const scheduleKeyboard = new InlineKeyboard()
+    .text("Понеділок 20:00", "monday").row()
+    .text("Вівторок 19:00", "tuesday").row()
+    .text("Середа 20:00", "wednesday").row()
+    .text("Четверг 19:00", "thursday").row()
+    .text("Пʼятниця 20:00", "friday").row()
+    .text('Показати мої записи', 'all_records');
+const backKeyboard = new InlineKeyboard()
+    .text('< Повернутись до меню', 'back');
+const recordedList = new Set();
 
 bot.command('start').filter((ctx) => {
     return ctx.msg.chat?.username === "Ad_Impossibilia_Nemo_Obligatur" //"jullibondarenko"
@@ -16,18 +29,39 @@ bot.command('start', async (ctx) => {
 });
 
 bot.on('message', async (ctx) => {
-    const scheduleLabels = ["Понеділок 20:00", "Вівторок 19:00", "Середа 20:00", "Четверг 19:00", "Пʼятниця 20:00"];
-    const scheduleOptions = scheduleLabels.map((label) => {
-        return [
-            Keyboard.text(label)
-        ]
-    });
-    const scheduleKeyboard = Keyboard.from(scheduleOptions).resized().oneTime();
-    
     await ctx.reply('Обери день для запису', {
         reply_markup: scheduleKeyboard
     });
 });
+
+bot.callbackQuery('all_records', async (ctx) => {
+    const scheduleOptionsText = [...recordedList] ? `Ви записані на ${[...recordedList]}` : 'Наразі записів не знайдено'; 
+    await ctx.callbackQuery.message.editText(`${scheduleOptionsText}`, {
+        reply_markup: backKeyboard,
+    });
+})
+
+bot.callbackQuery(['monday', 'tuesday', 'wednesday', 'thursday', 'friday'], async (ctx) => {
+    recordedList.add(ctx.callbackQuery.data);
+    console.log(recordedList);
+    await ctx.callbackQuery.message.editText(`Ви записались на ${ctx.callbackQuery.data}`, {
+        reply_markup: backKeyboard,
+    });
+    await ctx.answerCallbackQuery();
+});
+
+bot.callbackQuery('back', async (ctx) => {
+    await ctx.callbackQuery.message.editText('Обери день для запису', {
+        reply_markup: scheduleKeyboard,
+    });
+    await ctx.answerCallbackQuery();
+});
+
+// bot.on('callback_query:data', async (ctx) => {
+//     await ctx.answerCallbackQuery();
+//     console.log(ctx.callbackQuery.message.reply_markup.inline_keyboard.text);
+//     await ctx.reply(`Ви записались на ${ctx.callbackQuery.data}`);
+// });
 
 //Error handlers
 bot.catch((err) => {
