d0e93c776496a9635c8df13cf4c0022a789cbafd
Marharyta Hrinchenko <marharyta.hrinchenko@yalantis.net>
2024-05-30 15:12:00 +0100

    add logic

--- helper/getRecordsOptions.js
+++ helper/getRecordsOptions.js
@@ -1,5 +1,8 @@
-export const getRecordsOptions = (options, records) => {
-    return options
+export const getRecordsOptions = (options, records, additionalValue) => {
+    let result = options
         .filter(option => records.has(option.value))
-        .map(option => ({ value: option.value, label: option.label }));
+        .map(option => ({ value: `${option.value}_${additionalValue}`, label: option.label }));
+        
+    result.push({ value: 'back', label: '📍 Повернутись до меню' });
+    return result;
 }
