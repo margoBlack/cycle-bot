059a58d0b5f7456764eda7e015ce5a9d02d0a2a8
Marharyta Hrinchenko <marharyta.hrinchenko@yalantis.net>
2024-05-30 19:01:42 +0100

    add firebase files

--- index.js
+++ index.js
@@ -115,16 +115,14 @@ bot.catch((err) => {
     }
   });
 
-// bot.start();
-
 const firebaseConfig = {
-    apiKey: "AIzaSyArR5C-fSDQvdkaZW1cgpdiW7GRfj42JKw",
-    authDomain: "cycle-demo-88638.firebaseapp.com",
-    projectId: "cycle-demo-88638",
-    storageBucket: "cycle-demo-88638.appspot.com",
-    messagingSenderId: "752500047079",
-    appId: "1:752500047079:web:4216e306a864639c943642"
-};
+    apiKey: "AIzaSyA4BtOR_2xBX6vmuqES5a6qVfwfp3M3Cwo",
+    authDomain: "cycle-bot-997b0.firebaseapp.com",
+    projectId: "cycle-bot-997b0",
+    storageBucket: "cycle-bot-997b0.appspot.com",
+    messagingSenderId: "748030380319",
+    appId: "1:748030380319:web:83e0d7c7aa35bac646c82a"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
