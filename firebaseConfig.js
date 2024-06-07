e836e35e91abed6dccaaf6b80933f04aa5082eb5
Marharyta Hrinchenko <marharyta.hrinchenko@yalantis.net>
2024-06-07 16:32:40 +0300

    add config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,8 @@
-
-// Replace it with your FB config
-// Project settings -> General -> Your apps
-export const  firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-};
+export const firebaseConfig = {
+    apiKey: "AIzaSyA4BtOR_2xBX6vmuqES5a6qVfwfp3M3Cwo",
+    authDomain: "cycle-bot-997b0.firebaseapp.com",
+    projectId: "cycle-bot-997b0",
+    storageBucket: "cycle-bot-997b0.appspot.com",
+    messagingSenderId: "748030380319",
+    appId: "1:748030380319:web:83e0d7c7aa35bac646c82a"
+  };
